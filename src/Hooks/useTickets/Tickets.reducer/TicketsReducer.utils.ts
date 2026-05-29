6b8a84262238783c827aec014f6b442e4e188928
crotch-watch <naps_08_bay@icloud.com>
2026-05-29 23:56:52 +0530

    refac(reducer): main and filtered list are nominally typed now

--- src/Hooks/useTickets/Tickets.reducer/TicketsReducer.utils.ts
+++ src/Hooks/useTickets/Tickets.reducer/TicketsReducer.utils.ts
@@ -1,19 +1,33 @@
-import type { Ticket, TicketFilters } from "../Tickets.types"
+import type {
+  FilteredTickets,
+  FilteredTicketsPresent,
+  Ticket,
+  TicketFilters,
+  Tickets,
+  TicketsPresent
+} from "../Tickets.types"
 
 export function applyFilters(ticket: Ticket, filters: TicketFilters) {
-    const { searchTerm, domain, priority, status } = filters
+  const { searchTerm, domain, priority, status } = filters
 
-    const normalizedSearch = searchTerm.trim().toLowerCase()
+  const normalizedSearch = searchTerm.trim().toLowerCase()
 
-    const hasSearchTerm =
-        normalizedSearch === "" ||
-        Object.values(ticket).some(value => value != null && value.toString().toLowerCase().includes(normalizedSearch))
+  const hasSearchTerm =
+    normalizedSearch === "" ||
+    Object.values(ticket).some(value => value != null && value.toString().toLowerCase().includes(normalizedSearch))
 
-    const hasSpecifiedDomain = domain === "All Domains" || ticket.domain === domain
+  const hasSpecifiedDomain = domain === "All Domains" || ticket.domain === domain
 
-    const hasSpecifiedPriority = priority === "All Priorities" || ticket.priority === priority
+  const hasSpecifiedPriority = priority === "All Priorities" || ticket.priority === priority
 
-    const hasSpecifiedStatus = status === "All Statuses" || ticket.status === status
+  const hasSpecifiedStatus = status === "All Statuses" || ticket.status === status
 
-    return hasSearchTerm && hasSpecifiedDomain && hasSpecifiedPriority && hasSpecifiedStatus
+  return hasSearchTerm && hasSpecifiedDomain && hasSpecifiedPriority && hasSpecifiedStatus
 }
+
+export const ticketsArePresent = (tickets: Tickets): tickets is TicketsPresent => tickets.length > 0
+
+export const brand = <Source, Target>(source: Source) => source as unknown as Target
+
+export const filteredTicketsArePresent = (filteredTickets: FilteredTickets): filteredTickets is FilteredTicketsPresent =>
+  filteredTickets.length > 0
--- src/Hooks/useTickets/Tickets.reducer/addIncomingTicket.ts
+++ src/Hooks/useTickets/Tickets.reducer/addIncomingTicket.ts
@@ -1,36 +1,52 @@
-import { applyFilters } from "./TicketsReducer.utils"
+import { applyFilters, brand, filteredTicketsArePresent, ticketsArePresent } from "./TicketsReducer.utils"
 
-import { modes } from "../Tickets.consts"
-
-import type { FilteredTickets, TicketsState } from "../Tickets.types"
+import type { FilteredTicketsPresent, Ticket, TicketsPresent, TicketsState } from "../Tickets.types"
 import type { AddTicketAction } from "./Reducer.types"
 
 export const addIncomingTicket = (state: TicketsState, payload: AddTicketAction["payload"]): TicketsState => {
-    const incomingTicket = payload
+  const incomingTicket = payload
 
-    const {
-        data,
-        view: { filters, filteredTickets }
-    } = state
+  const { data, view } = state
+  const { filters, filteredTickets } = view
 
-    const idAlreadyExists = data.list.some(ticket => ticket.id === incomingTicket.id)
+  if (!ticketsArePresent(data)) {
+    return {
+      ...state,
+      mode: "viewing",
+      data: brand<Array<Ticket>, TicketsPresent>([incomingTicket]),
+      view: { ...state.view, ticketsBeingEdited: [] }
+    }
+  } else {
+    const idAlreadyExists = data.some(ticket => ticket.id === incomingTicket.id)
     if (idAlreadyExists) return state
 
-    const updatedTickets = [incomingTicket, ...data.list]
-
     const passedFilters = applyFilters(incomingTicket, filters)
-    const updatedFiltersTickets: FilteredTickets = {
-        type: "filtered",
-        list: passedFilters ? [incomingTicket, ...filteredTickets.list] : [...filteredTickets.list]
+
+    if (!passedFilters) {
+      return {
+        ...state,
+        mode: "viewing",
+        data: brand<Array<Ticket>, TicketsPresent>([incomingTicket, ...data]),
+        view: {
+          ...state.view,
+          ticketsBeingEdited: []
+        }
+      }
     }
+
+    const updatedFilteredTickets = filteredTicketsArePresent(filteredTickets)
+      ? [incomingTicket, ...filteredTickets]
+      : [incomingTicket]
 
     return {
-        mode: modes.viewing,
-        data: { type: "master", list: updatedTickets },
-        view: {
-            ...state.view,
-            filteredTickets: updatedFiltersTickets,
-            ticketsBeingEdited: { type: "editing", list: [] }
-        }
+      ...state,
+      mode: "viewing",
+      data: brand<Array<Ticket>, TicketsPresent>([incomingTicket, ...data]),
+      view: {
+        ...state.view,
+        ticketsBeingEdited: [],
+        filteredTickets: brand<Array<Ticket>, FilteredTicketsPresent>(updatedFilteredTickets)
+      }
     }
+  }
 }
